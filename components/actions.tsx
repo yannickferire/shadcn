8aec5ccfe0331759340e27eab58a6a624be133e6
yannickferire <yannick.ferire@gmail.com>
2023-06-13 10:39:31 +0200

    Start of template for model

---
+++ components/actions.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export function Actions() {
+  return (
+    <>
+      <div className="flex gap-4">
+        <Button variant="primary">Enregistrer</Button>
+      </div>
+    </>
+  )
+}
---
+++ components/model.tsx
@@ -0,0 +1,59 @@
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Checkbox } from "@/components/ui/checkbox"
+
+export function Model() {
+  return (
+    <>
+      <div className="flex gap-4">
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="model_name">Modèle</Label>
+          <Input type="text" id="model_name" placeholder="Austral" maxlength={50} />
+        </div>
+        <div className="grid w-50 max-w-sm items-center gap-1.5">
+          <Label htmlFor="model_code">Code Modèle</Label>
+          <Input type="text" id="model_name" placeholder="RN1" maxlength={3} />
+        </div>
+      </div>
+      <div className="flex gap-8">
+        <div className="flex items-center space-x-2">
+          <Checkbox id="new" />
+          <Label htmlFor="new">Nouveau</Label>
+        </div>
+        <div className="flex items-center space-x-2">
+          <Checkbox id="male" />
+          <Label htmlFor="male">Masculin</Label>
+        </div>
+      </div>
+      <div className="flex gap-2 flex-col">
+        <p className="text-sm text-gray-500">Catégories</p>
+        <div className="flex items-center space-x-2">
+          <Checkbox id="cat_vt" />
+          <Label htmlFor="cat_vt">Thermique</Label>
+        </div>
+        <div className="flex items-center space-x-2">
+          <Checkbox id="cat_vh" />
+          <Label htmlFor="cat_vh">Hybride</Label>
+        </div>
+        <div className="flex items-center space-x-2">
+          <Checkbox id="cat_vph" />
+          <Label htmlFor="cat_vph">Plug-in Hybride</Label>
+        </div>
+        <div className="flex items-center space-x-2">
+          <Checkbox id="cat_ve" />
+          <Label htmlFor="cat_ve">Électrique</Label>
+        </div>
+      </div>
+      <div className="flex gap-4">
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="consumption">Consommation</Label>
+          <Input type="text" id="consumption" placeholder="4,6 - 4,9" />
+        </div>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="emission">Emission CO<sub>2</sub></Label>
+          <Input type="text" id="emission" placeholder="104 - 110" />
+        </div>
+      </div>
+    </>
+  )
+}
